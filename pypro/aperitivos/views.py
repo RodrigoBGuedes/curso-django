79c355d3fd3c1ebeffc77fc2b7702cc91fa7a941
rodrigo.guedes <gedsao77@hotmail.com>
2022-09-28 16:13:11 -0300

    Implementada exibição do segundo vídeo aperitivo

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': '754804288?h=243c51ec88'}
+    videos = {
+        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': '754804288?h=243c51ec88'},
+        'instalacao-windows': {'titulo': 'Instalação Windows', 'vimeo_id': '251497668'},
+    }
+    video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
