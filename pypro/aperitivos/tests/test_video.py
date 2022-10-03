ba6a4ed08450cdc30251754ad39670289cc591c6
rodrigo.guedes <gedsao77@hotmail.com>
2022-10-03 20:31:39 -0300

    Instalada lib Model Mommy

--- pypro/aperitivos/tests/test_video.py
+++ pypro/aperitivos/tests/test_video.py
@@ -1,15 +1,13 @@
 import pytest
 from django.urls import reverse
-
+from model_mommy import mommy
 from pypro.aperitivos.models import Video
 from pypro.django_assertions import assert_contains
 
 
 @pytest.fixture
 def video(db):
-    v = Video(slug='motivacao', titulo='Video Aperitivo: Motivação', vimeo_id='754804288?h=243c51ec88')
-    v.save()
-    return v
+    return mommy.make(Video)
 
 
 @pytest.fixture
