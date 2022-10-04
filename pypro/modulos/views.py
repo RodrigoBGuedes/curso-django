961b0aef057d62063d2ee9ee0f059aa4b8411375
rodrigo.guedes <gedsao77@hotmail.com>
2022-10-04 17:14:16 -0300

    Criados links para aulas

--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -6,4 +6,8 @@ from pypro.modulos import facade
 def detalhe(request, slug):
     modulo = facade.encontrar_modulo(slug)
     aulas = facade.listar_aulas_de_mpdulo_ordenados(modulo)
-    return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas':aulas})
+    return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas': aulas})
+
+
+def aulas(request, slug):
+    pass
