8b5edaac4456efb1bc2bd80cbbd9794576251d1e
rodrigo.guedes <gedsao77@hotmail.com>
2022-10-06 11:13:01 -0300

    Finalizada funcionalidade de matrícula

--- pypro/turmas/admin.py
+++ pypro/turmas/admin.py
@@ -3,10 +3,17 @@ from django.contrib import admin
 from pypro.turmas.models import Turma
 
 
-# Register your models here.
+class MatriculaInline(admin.TabularInline):
+    model = Turma.alunos.through
+    extra = 1
+    readonly_fields = ('data',)
+    autocomplete_fields = ('usuario',)
+    ordering = ('-data',)
+
 
 @admin.register(Turma)
 class TurmaAdmin(admin.ModelAdmin):
+    inlines = [MatriculaInline]
     list_display = ('nome', 'slug', 'inicio', 'fim')
     prepopulated_fields = {'slug': ('nome',)}
     ordering = ('-inicio',)
