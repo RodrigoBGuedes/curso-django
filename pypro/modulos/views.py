fe396f27a16f60da192e1b69e2fbca8c767d1dd8
rodrigo.guedes <gedsao77@hotmail.com>
2022-10-06 14:37:00 -0300

    Implementados Login e Logout

--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 from pypro.modulos import facade
 
 
@@ -7,12 +7,14 @@ def indice(request):
     ctx = {'modulos': facade.listar_modulos_com_aulas()}
     return render(request, 'modulos/indice.html', ctx)
 
+
 def detalhe(request, slug):
     modulo = facade.encontrar_modulo(slug)
     aulas = facade.listar_aulas_de_mpdulo_ordenados(modulo)
     return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas': aulas})
 
 
+@login_required
 def aula(request, slug):
     aula = facade.encontrar_aula(slug)
     return render(request, 'modulos/aula_detalhe.html', {'aula': aula})
